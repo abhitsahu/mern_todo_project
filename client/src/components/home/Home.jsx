4cd460424828f3f92df183f952911d0804e0e38c
abhit <sahuabhit27@gmail.com>
2025-02-12 12:01:21 +0530

    updated

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <h1>Welcome to Our Website!</h1>
             <p>Your one-stop solution for all your needs.</p>
             <button className="cta-button">
-                <Link to="/signup">Get Started</Link>
+                <Link to="/todo">Get Started</Link>
             </button>
         </div>
     </div>
